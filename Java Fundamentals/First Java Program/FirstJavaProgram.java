e23024f4af4a727d99fb62897fa1b49cafb4871b
uditpatil <uditnp08@gmail.com>
2023-01-08 15:35:32 -0800

    java fundamentals 1st commit

---
+++ Java Fundamentals/First Java Program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My Name is Udit Patil");
+        System.out.println("I am 30 years old");
+        System.out.println("My hometown is Santa Clara, CA");
+    }
+}
